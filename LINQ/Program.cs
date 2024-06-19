cd0aa850cab2528e3cb19e8e27d43a2f7e9b3492
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-19 10:14:14 -0700

    list of Vgames sorted length

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Madden",
+                "FIFA",
+                "NHL",
+                "Fortnite",
+                "WarZone",
+                "Fall Guys"
+
+            };
+            List<string> sortedGames = videoGames.OrderBy(game => game.Length).ToList();
+
+            foreach (string game in sortedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
+
     }
 }
